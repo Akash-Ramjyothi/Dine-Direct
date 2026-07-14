4a2ea31e8789caa9817950daf065091288a8e08e
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-07-14 21:40:07 +0530

    Opened Cart in Model with context

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,13 +1,17 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom"
 
 export default function Modal({ children, open, className = '' }) {
     const dialog = useRef();
 
     useEffect(() => {
+        const modal = dialog.current;
+
         if (open) {
-            dialog.current.showModal();
+            modal.showModal();
         }
+
+        return () => modal.close();
     }, [open]);
 
     return createPortal(
